4f9b8b6076e6116a48609a651188b58dab692b95
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-01-07 17:49:59 +0100

    added login and signup routes

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import Users from '../models/users'
+import jwt from "jsonwebtoken";
+import { secret } from "../config/environment";
+
 
 export async function getUsers(req: Request, res: Response) {
   try {
@@ -9,5 +12,34 @@ export async function getUsers(req: Request, res: Response) {
   } catch (error) {
     console.log(error)
     res.send({ message: "There was an error getting all users" })
+  }
+}
+
+export async function signUp(req: Request, res: Response) {
+  try {
+    const user = await Users.create(req.body)
+    res.send(user)
+  } catch (error) {
+    console.log(error)
+    res.send({ message: "There was an error signing up" })
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  try {
+    const user = await Users.findOne({ email: req.body.email })
+    if (!user) {
+      console.log('no user found')
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      secret,
+      { expiresIn: '24h' }
+    )
+    res.send({ message: "Login successful", token })
+
+  } catch (error) {
+    console.log(error)
+    res.send({ message: "Login failed" })
   }
 }
